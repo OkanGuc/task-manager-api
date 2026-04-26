5cc06c65c400c198178ca146549a7456b24f9ac9
OkanG <146350564+OkanGuc@users.noreply.github.com>
2026-04-26 13:27:54 +0200

    use env variables

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const express = require('express')
 const mysql = require('mysql2')
 const cors = require('cors')
 
-
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'okan1234',
-  database: 'taskmanager'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 })
-
 db.connect((err) => {
   if (err) {
     console.error('Erreur connexion MySQL:', err)
